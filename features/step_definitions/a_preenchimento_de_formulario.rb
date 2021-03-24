8661e14d563f4b278322677af16216b0a05b73e0
julio Santos <julio.santos@i9xp.com.br>
2021-03-24 15:40:52 -0300

    mensagem

--- features/step_definitions/a_preenchimento_de_formulario.rb
+++ features/step_definitions/a_preenchimento_de_formulario.rb
@@ -1,3 +1,4 @@
+#Cenario: cadastro de usuario 
 Dado('que eu acesso o site de automação') do
     visit '/'
     click_on 'Começar Automação Web'
@@ -10,21 +11,20 @@ end
 
 Quando('preencho as minhas informações') do |table|
     register = table.rows_hash
-    find('#user_name').set register[:nome]
-    find('#user_lastname').set register[:segundo_nome]
-    find('#user_email').set register[:email]
-    find('#user_address').set register[:endereço]
-    find('#user_university').set register[:universidade]
-    find('#user_profile').set register[:profissão]
-    find('#user_gender').set register[:genero]
-    find('#user_age').set register[:idade]
+    find('#user_name').set register       [:nome        ] #Nomde do usuário
+    find('#user_lastname').set register   [:segundo_nome] #Segundo nome do usuário 
+    find('#user_email').set register      [:email       ] #email do usuário 
+    find('#user_address').set register    [:endereço    ] #endereço do usuário
+    find('#user_university').set register [:universidade] #universidade onde estuda 
+    find('#user_profile').set register    [:profissão   ] #profissão do usuário
+    find('#user_gender').set register     [:genero      ] #Sexo do usuário 
+    find('#user_age').set register        [:idade       ] #idade do usuário 
     find('input[type="submit"]').click
     sleep 0
 end
 
 Então('eu realizo o cadastro da conta') do
     expect(page).to have_content("Usuário Criado com sucesso")
-
 end
 
 #Cenario: visualizar lista de usuarios 
@@ -38,5 +38,4 @@ end
 
 Então('eu visualizo todas as contas cadastradas') do
     expect(page).to have_content("Lista de Usuários!!")
- 
 end
