6c49e002cbb7246d20c8b4c12faeaf65c4057971
julio Santos <julio.santos@i9xp.com.br>
2021-03-29 22:52:47 -0300

    tests autoomation

--- features/step_definitions/mudanca_de_foco.rb
+++ features/step_definitions/mudanca_de_foco.rb
@@ -1,7 +1,14 @@
 #Cenario: Clicando em alert  
 Dado('que eu acesso o site automação') do 
     @mudanca_de_foco_Page = Mudancadefocopage.new
-    visit '/mudancadefoco/alert'
+    visit '/'
+    click_on 'Começar Automação Web'
+end
+Quando('eu clico em mudança de foco') do 
+end
+Quando('clico em algum botão') do
+    @mudanca_de_foco_Page.clico_em_mudanca_de_foco.click
+    @mudanca_de_foco_Page.clico_em_alert.click
     @mudanca_de_foco_Page.clica_js_alert.click          
     page.accept_alert                                    # -> Clica em alerts 
     @mudanca_de_foco_Page.clica_js_confirm.click       
@@ -9,16 +16,14 @@ Dado('que eu acesso o site automação') do
     @mudanca_de_foco_Page.clica_js_prompt.click   
     page.driver.browser.switch_to.alert.accept           # -> Clica em alerts com texto
 end
-Quando('eu clico em mudança de foco') do 
-end
-Quando('clico em algum botão') do
-end
 Então('eu consigo clicar nos botões da tela') do
 end
 
 #Cenario: Clicando em botões infra
 Quando('clico em botões iframe') do 
-    visit '/mudancadefoco/iframe'
+    
+    @mudanca_de_foco_Page.clico_em_mudanca_de_foco.click
+    @mudanca_de_foco_Page.clico_em_iframe.click
     within_frame('id_do_iframe') do 
     fill_in(id:'first_name', with:                  'Julio')
     fill_in(id:'last_name',  with:                 'Santos')
@@ -33,3 +38,32 @@ end
 Então('eu consigo preencher campos no iframe') do
     expect(page).to have_content 'Trabalhando com Iframe'
 end
+
+#Cenario: Validar que seja aberta uma nova aba 
+Quando('clico em abrir uma janela') do
+    visit 'mudancadefoco/janela'
+    janela = window_opened_by do
+        click_link 'Clique aqui' 
+    end 
+  
+    within_window janela do
+
+        expect(current_url).to eq 'https://automacaocombatista.herokuapp.com/mudancadefoco/newwindow'
+        @mensagem = find('.red-text.text-darken-1.center')
+        expect(@mensagem.text).to eq 'Você Abriu uma nova janela!!'
+    end 
+end
+
+Então('é aberto uma nova aba no navegador') do
+end 
+
+#Cenario: Validar que seja aberta clique em modal 
+Quando('clico em modal') do
+ visit '/mudancadefoco/modal'
+ @mudanca_de_foco_Page.clico_em_abrir.click
+end
+
+Então('eu vejo o modal') do
+    expect(page).to have_content 'Modal Teste'
+end
+
